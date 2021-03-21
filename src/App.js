44d99ac7d76b96238d13786e3f7e1b388872317c
Derrick Strong <derrickmstrong@gmail.com>
2021-03-20 22:03:34 -0500

    Added Star Rating component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const createArray = (length) => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 4 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} out of {totalStars}
+      </p>
+    </>
+  );
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-  const handleClick = () => {
-    setChecked((checked) => !checked);
-  };
   return (
-    <div>
-      <input type="checkbox" value={checked} onClick={handleClick} />
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
+    <>
+      <StarRating totalStars={4} />
+    </>
   );
 }
 
