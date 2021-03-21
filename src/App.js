43d0f1ae7fb889695432dd64a433d7b77a0185fb
Derrick Strong <derrickmstrong@gmail.com>
2021-03-21 10:09:19 -0500

    Basic useEffect Hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
-
-const createArray = (length) => [...Array(length)];
-
-function Star({ selected = false, onSelect }) {
-  return <FaStar color={selected ? "red" : "gray"} onClick={onSelect} />;
-}
-
-function StarRating({ totalStars = 4 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))}
-      <p>
-        {selectedStars} out of {totalStars}
-      </p>
-    </>
-  );
-}
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Derrick");
+  const handleClick = () => setName("Rick");
+
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  }, [name]);
+
   return (
-    <>
-      <StarRating totalStars={4} />
-    </>
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={handleClick}>Change Winner</button>
+    </section>
   );
 }
 
