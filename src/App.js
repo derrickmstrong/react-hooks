cfcae965ae4ec27290e44893c7c554a794bf034b
Derrick Strong <derrickmstrong@gmail.com>
2021-03-22 14:37:21 -0500

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import React, { Fragment, useReducer } from "react";
-
-const initialState = {
-  message: "Hi",
-};
-
-// Reducer takes in state and action and switch based on action type
-function reducer(state, action) {
-  // action is some type of an event
-  switch (action.type) {
-    case "yell":
-      return {
-        // Below state.message will return the previous state and can be helpf ul in calculations
-        message: "HEY! I JUST SAID " + state.message,
-      };
-    case "whisper":
-      return {
-        message: "excuse me",
-      };
-  }
-}
+import React, { useRef } from "react";
 
 function App() {
-  // useReducer(Takes in a function that will return a new state, the inital state)
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const sound = useRef();
+  const color = useRef();
 
+  const submit = (e) => {
+    e.preventDefault();
+    const soundVal = sound.current.value;
+    const colorVal = color.current.value;
+    alert(`${soundVal} sounds like ${colorVal}`);
+    sound.current.value = "";
+    color.current.value = "";
+  };
   return (
-    <Fragment>
-      <p>Message {state.message} </p>
-      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
-      <button onClick={() => dispatch({ type: "whisper" })}>excuse me</button>
-    </Fragment>
+    <form onSubmit={submit}>
+      <input ref={sound} type="text" placeholder="Enter a sound" />
+      <input ref={color} type="color" />
+      <button>Add</button>
+    </form>
   );
 }
 
