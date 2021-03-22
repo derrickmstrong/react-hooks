bd4d985cda2de1f722b1ef6982959cf9b3ccaf8b
Derrick Strong <derrickmstrong@gmail.com>
2021-03-22 11:14:27 -0500

    Basic useReducer Hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useReducer } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
+  // useReducer(Takes in a function that will return a new state, the inital state)
+  const [number, setNumber] = useReducer(
+    (number, newNumber) => number + newNumber,
+    0
+  );
 
-  useEffect(() => {
-    // Fetch data
-    fetch(`https://api.github.com/users`)
-      // Parse response to json
-      .then((res) => res.json())
-      // Set data to response results
-      .then((res) => setData(res));
-    return () => {};
-  }, []);
-
-  const handleClick = () => {
-    setData([]);
-  };
-
-  if (data) {
-    return (
-      <>
-        <ul>
-          {data.map(({ login, avatar_url, id }) => (
-            <li key={id} style={{ listStyle: "none" }}>
-              <img src={avatar_url} width="50" />
-              <br />
-              {login}
-              <br />
-              <br />
-            </li>
-          ))}
-        </ul>
-        <button onClick={handleClick}>Remove Data</button>
-      </>
-    );
-  }
-  return "No data";
+  return <h1 onClick={() => setNumber(1)}>{number}</h1>;
 }
 
 export default App;
