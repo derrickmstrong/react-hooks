6e18d893d49acccbc31935012e2ecc47a5c55d6c
Derrick Strong <derrickmstrong@gmail.com>
2021-03-22 14:27:57 -0500

    Advanced useReducer hook with actions and dispatch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Fragment, useReducer } from "react";
 
+const initialState = {
+  message: "Hi",
+};
+
+// Reducer takes in state and action and switch based on action type
+function reducer(state, action) {
+  // action is some type of an event
+  switch (action.type) {
+    case "yell":
+      return {
+        // Below state.message will return the previous state and can be helpf ul in calculations
+        message: "HEY! I JUST SAID " + state.message,
+      };
+    case "whisper":
+      return {
+        message: "excuse me",
+      };
+  }
+}
+
 function App() {
   // useReducer(Takes in a function that will return a new state, the inital state)
-  const [checked, toggle] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <Fragment>
-      <input type="checkbox" value={checked} onChange={toggle} />
-      <p>{checked ? "checked" : "unchecked"}</p>
+      <p>Message {state.message} </p>
+      <button onClick={() => dispatch({ type: "yell" })}>YELL</button>
+      <button onClick={() => dispatch({ type: "whisper" })}>excuse me</button>
     </Fragment>
-  )
+  );
 }
 
 export default App;
